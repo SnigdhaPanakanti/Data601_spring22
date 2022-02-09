1c837c1176da88019602e24760e945a86263bcb6
Snigdha Panakanti <spanakanti25@gmail.com>
2022-02-09 17:34:29 -0500

    <message>

---
+++ week01/My_script.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+fname= "Snigdha"
+sname= "Panakanti"
+fullname = fname +  sname
+
+def Name():
+    print("Fullname:", fullname)
+    
+Name()
+
+
+# In[7]:
+
+
+import os
+cwd = os.getcwd()
+print(cwd)
+
+
+# In[ ]:
+
+
+
+
